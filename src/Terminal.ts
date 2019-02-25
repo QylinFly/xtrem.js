239eb2574aa0772dd85315803cdb3f680ac0d624
Qylin <18612116114@163.com>
2019-02-25 14:16:19 +0800

    解决手机输入02

--- src/Terminal.ts
+++ src/Terminal.ts
@@ -1681,29 +1681,31 @@ export class Terminal extends EventEmitter
       this.selectionManager.selectLines(start, end)
     }
   }
-  protected _isMobile = function () 
-  {
-      var browser = {  
-          versions: function() {  
-              var u = navigator.userAgent, app = navigator.appVersion;  
-              return {     //移动终端浏览器版本信息  
-                  trident: u.indexOf('Trident') > -1, //IE内核  
-                  presto: u.indexOf('Presto') > -1, //opera内核  
-                  webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核  
-                  gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核  
-                  mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端  
-                  ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端  
-                  android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器  
-                  iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器  
-                  iPad: u.indexOf('iPad') > -1, //是否iPad  
-                  webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部  
-              };  
-          } (),  
-          language: (navigator.browserLanguage || navigator.language).toLowerCase()  
-      }  
-      console.log(browser)
-      if (browser.versions.mobile) {//判断是否是移动设备打开。browser代码在下面  
-          /* var ua = navigator.userAgent.toLowerCase();//获取判断用的对象 
+  protected _isMobile = function() {
+    var browser = {
+      versions: (function() {
+        var u = navigator.userAgent // , app = navigator.appVersion;
+        return {
+          //移动终端浏览器版本信息
+          trident: u.indexOf('Trident') > -1, //IE内核
+          presto: u.indexOf('Presto') > -1, //opera内核
+          webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+          gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+          mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+          ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+          android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+          iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+          iPad: u.indexOf('iPad') > -1, //是否iPad
+          webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+        }
+      })()
+      // ,
+      // language: (navigator.browserLanguage || navigator.language).toLowerCase()
+    }
+    console.log(browser)
+    if (browser.versions.mobile) {
+      //判断是否是移动设备打开。browser代码在下面
+      /* var ua = navigator.userAgent.toLowerCase();//获取判断用的对象 
           if (ua.match(/MicroMessenger/i) == "micromessenger") { 
           //在微信中打开 
           setInterval(WeixinJSBridge.call('closeWindow'),2000); 
@@ -1719,20 +1721,20 @@ export class Terminal extends EventEmitter
           }  
           if(browser.versions.android){ 
           //是否在安卓浏览器打开 
-          }*/  
-          return true
-      }  
+          }*/
+
+      return true
+    }
+    return false
+  }
+  protected _isPrint = function(code: Number) {
+    if (code >= 32 && code <= 126) {
+      return true
+    } else if (code >= 186 && code <= 222) {
+      return true
+    } else {
       return false
-  };
-  protected _isPrint = function (code: Number) {
-      if(code >= 32 && code <= 126){
-          return true
-      }else if(code >= 186 && code <= 222){
-          return true
-      }
-      else{
-          return false
-      }
+    }
   }
   /**
    * Handle a keydown event
@@ -1784,9 +1786,9 @@ export class Terminal extends EventEmitter
       return true
     }
     // 解决手机输入
-    if(this._isMobile() && this._isPrint(event.keyCode)){
-      this.handler(event.key);
-  }
+    if (this._isMobile() && this._isPrint(event.keyCode)) {
+      this.handler(event.key)
+    }
     if (result.cancel) {
       // The event is canceled at the end already, is this necessary?
       this.cancel(event, true)
